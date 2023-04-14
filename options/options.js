50dd655658a31be803191319664daea535e28e1a
Crowdbotics <team@crowdbotics.com>
2023-04-14 14:45:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tranquil-library-39936",
+  url: "https://tranquil-library-39936.botics.co",
+  api: "https://tranquil-library-39936.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
